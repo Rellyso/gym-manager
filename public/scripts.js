5d1c0aa5da915b8fbd24339b11f9cc53bbfc2376
Rellyson <rellysondouglas2015@gmail.com>
2020-06-30 10:18:20 -0300

    Init of website

---
+++ public/scripts.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener('click', function(){
+        const videoID = card.getAttribute('id')
+        window.location.href = `/video?id=${videoID}`
+    })
+}
+
+
+
