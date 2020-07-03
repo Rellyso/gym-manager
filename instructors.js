d00319e296cf8f434d2ed74f30aca9127ba52581
Rellyson <rellysondouglas2015@gmail.com>
2020-07-03 10:24:24 -0300

    Added local data

---
+++ instructors.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const data = require('./data')
+
+// create
+exports.post = function (req, res) {
+    // GET = req.query
+    // POST = req.body -> para habilitar deve-se usar o express.urlencoded({ extended: true})
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        // req.body.avatar_url == ""
+        if (req.body[key] == "")
+            return res.send('Please fill in all fields.')
+    }
+    
+    // desestruturando o req.body
+    let {avatar_url, name, birth, gender, services} = req.body
+
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 4), function (err) {
+        if (err) return res.send('Write file error.')
+
+        return res.redirect('/instructors')
+    })
+
+    // return res.send(req.body)
+}
+
+// update 
+
+// delete
