51d0c6d02447afff74928914297abe34746908e9
Rellyson <rellysondouglas2015@gmail.com>
2020-07-22 07:53:26 -0300

    Getting new functions like delete

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -69,6 +69,8 @@ module.exports = {
         })
     },
     delete(req, res) {
-        return
+        Instructor.delete(req.body.id, function () {
+            return res.redirect('/instructors')
+        })
     },
 }
