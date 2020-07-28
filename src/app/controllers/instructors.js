6ef06976e0edba13bc418f669e28a0390901f8f3
Rellyson <rellysondouglas2015@gmail.com>
2020-07-27 21:39:45 -0300

    Getting more params now

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -4,35 +4,58 @@ const { age, date } = require('../../lib/utils')
 module.exports = {
     index(req, res) {
 
-        const { filter } = req.query
+        let { filter, page, limit } = req.query
 
-        if (filter) {
-            Instructor.findBy(filter, function (instructors) {
+        page = page || 1
+        limit = limit || 2
 
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(instructors) {
                 for (let i = 0; i < instructors.length; i++) {
                     instructors[i] = {
                         ...instructors[i],
                         services: instructors[i].services.split(',')
                     }
                 }
-
                 return res.render('instructors/index', { instructors, filter })
-            })
-        } else {
-
-            Instructor.all(function (instructors) {
-
-                for (let i = 0; i < instructors.length; i++) {
-                    instructors[i] = {
-                        ...instructors[i],
-                        services: instructors[i].services.split(',')
-                    }
-                }
-
-
-                return res.render('instructors/index', { instructors })
-            })
+            }
         }
+
+        Instructor.paginate(params)
+
+        // if (filter) {
+        //     Instructor.findBy(filter, function (instructors) {
+
+        //         for (let i = 0; i < instructors.length; i++) {
+        //             instructors[i] = {
+        //                 ...instructors[i],
+        //                 services: instructors[i].services.split(',')
+        //             }
+        //         }
+
+        //         return res.render('instructors/index', { instructors, filter })
+        //     })
+        // } else {
+
+        //     Instructor.all(function (instructors) {
+
+        //         for (let i = 0; i < instructors.length; i++) {
+        //             instructors[i] = {
+        //                 ...instructors[i],
+        //                 services: instructors[i].services.split(',')
+        //             }
+        //         }
+
+
+        //         return res.render('instructors/index', { instructors })
+        //     })
+        // }
 
     },
     create(req, res) {
